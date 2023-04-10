8aff852e5b1da27861cf70882fe26eaa0545d76e
“Raizo-n” <sanpincha0826@gmail.com>
2023-04-10 00:24:05 -0700

    add: useForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,63 @@
 import "./App.css";
 import { useForm } from "react-hook-form";
 
+interface LoginForm {
+  name: string;
+  email: string;
+  password: string;
+}
+
 function App() {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm<LoginForm>({ mode: "onChange" });
 
-  const onSubmit = (data: any) => {
+  const onSubmit = (data: LoginForm) => {
     console.log(data);
-
-  }
+  };
 
   return (
     <div className="form-container">
       <form onSubmit={handleSubmit(onSubmit)}>
         <h1>Login</h1>
         <label htmlFor="name">Name</label>
-        <input id="name" type="text" />
+        <input
+          id="name"
+          type="text"
+          {...register("name", {
+            required: "You must type your name.",
+            minLength: {
+              value: 3,
+              message: "Your name must be at least 3 characters.",
+            },
+          })}
+        />
+        <p>{errors.name?.message as React.ReactNode}</p>
+
         <label htmlFor="email">Email</label>
-        <input id="email" type="email" />
+        <input
+          id="email"
+          type="email"
+          {...register("email", { required: "You must type your Email." })}
+        />
+        <p>{errors.email?.message as React.ReactNode}</p>
+
         <label htmlFor="password">Password</label>
-        <input id="password" type="password" />
+        <input
+          id="password"
+          type="password"
+          {...register("password", {
+            required: "You must type your password.",
+            minLength: {
+              value: 8,
+              message: "Your password must be at least 8 characters.",
+            },
+          })}
+        />
+        <p>{errors.password?.message as React.ReactNode}</p>
+
         <button type="submit">Submit</button>
       </form>
     </div>
